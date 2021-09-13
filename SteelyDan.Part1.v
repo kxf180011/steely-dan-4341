46fc93653d0f9b917c9ce12918ac77e57b4040a4
elp180002 <80294396+elp180002@users.noreply.github.com>
2021-09-12 22:34:57 -0500

    Update SteelyDan.Part1.v

--- SteelyDan.Part1.v
+++ SteelyDan.Part1.v
@@ -1,6 +1,5 @@
-//----------------------------------------------------------------------
 module Breadboard	(w, x, y, z, r0, r1, r2, r3, r4, r5, r6, r7, r8, r9);
-input w,x,y,z;
+input w, x, y, z;
 output r0, r1, r2, r3, r4, r5, r6, r7, r8, r9;
 reg r0, r1, r2, r3, r4, r5, r6, r7, r8, r9;
 
@@ -42,41 +41,36 @@ end
 
 endmodule
 
-//----------------------------------------------------------------------
-
 module testbench();
 
-  
   reg [4:0] i;
-  reg  a;
-  reg  b;
-  reg  c;
-  reg  d;
-  
+  reg a, b, c, d;
+
   wire  f0, f1, f2, f3, f4, f5, f6, f7, f8, f9;
   
-  Breadboard zap(a, b, c, d, f0, f1, f2, f3, f4, f5, f6, f7, f8, f9);
+  Breadboard part1(a, b, c, d, f0, f1, f2, f3, f4, f5, f6, f7, f8, f9);
 
   initial begin
 
-  $display ("|##|A|B|C|D|F0|F1|F2|F3|F4|F5|F6|F7|F8|F9|");
-  $display ("|==+=+=+=+=+==+==+==+==+==+==+==+==+==+==|");
+    $display ("|##|A|B|C|D|F0|F1|F2|F3|F4|F5|F6|F7|F8|F9|");
+    $display ("|==+=+=+=+=+==+==+==+==+==+==+==+==+==+==|");
 
-	for (i = 0; i < 16; i = i + 1) 
-	begin
+	  for (i = 0; i < 16; i = i + 1) 
+	    begin
 
-		a=(i/8)%2;
-		b=(i/4)%2;
-		c=(i/2)%2;
-		d=(i/1)%2;
+	    	a=(i/8)%2;
+	    	b=(i/4)%2;
+	    	c=(i/2)%2;
+    		d=(i/1)%2;
 		 
-		#20;
+	  	#20;
 		 	
-		$display ("|%2d|%1d|%1d|%1d|%1d| %1d| %1d| %1d| %1d| %1d| %1d| %1d| %1d| %1d| %1d|", i, a, b, c, d, f0, f1, f2, f3, f4, f5, f6, f7, f8, f9);
-		if(i%4==3)
-      $display ("|--+-+-+-+-+--+--+--+--+--+--+--+--+--+--|");
+	    	$display ("|%2d|%1d|%1d|%1d|%1d| %1d| %1d| %1d| %1d| %1d| %1d| %1d| %1d| %1d| %1d|", i, a, b, c, d, f0, f1, f2, f3, f4, f5, f6, f7, f8, f9);
+		
+        if(i%4==3)
+         $display ("|--+-+-+-+-+--+--+--+--+--+--+--+--+--+--|");
 
-	end
+  	end
  
 	#20;
 	$finish;
