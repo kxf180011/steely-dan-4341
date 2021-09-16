7779beaf893dd5a74ec35b111bce62a04a587809
elp180002 <80294396+elp180002@users.noreply.github.com>
2021-09-16 01:00:43 -0500

    Update Steely Dan.Part1.v

--- pt1/Steely Dan.Part1.v
+++ pt1/Steely Dan.Part1.v
@@ -52,7 +52,7 @@ module testbench();
 
  	wire  f0, f1, f2, f3, f4, f5, f6, f7, f8, f9;
   
-	Breadboard part1(a, b, c, d, f0, f1, f2, f3, f4, f5, f6, f7, f8, f9);
+	Breadboard stimulus(a, b, c, d, f0, f1, f2, f3, f4, f5, f6, f7, f8, f9);
 
 	initial begin
 
