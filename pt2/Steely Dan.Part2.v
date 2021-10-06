ecec9b706369eac7a86f095f14a4fca9bd3ede3c
elp180002 <80294396+elp180002@users.noreply.github.com>
2021-10-05 19:19:18 -0500

    Update Steely Dan.Part2.v

--- pt2/Steely Dan.Part2.v
+++ pt2/Steely Dan.Part2.v
@@ -14,10 +14,10 @@ module FullAdder(A,B,C,carry,sum);
 
 	always @(*) 
 	  begin
-	    sum=s1;//
-		sum= A^B^C;
-	    carry=c1|c0;//
-		carry= ((A^B)&C)|(A&B);  
+	    sum=s1;
+		sum=A^B^C;
+	    carry=c1|c0;
+		carry=((A^B)&C)|(A&B);  
 	  end
 	
 endmodule
@@ -29,21 +29,45 @@ module ADD_SUB(inputA,inputB,mode,sum,carry,overflow);
     output [31:0] sum;
 	output carry;
     output overflow;
-    wire b0,b1,b2,b3; //XOR Interfaces
-	wire c0,c1,c2,c3,c4; //Carry Interfaces
+    wire b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15; //XOR Interfaces
+	wire c0,c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16; //Carry Interfaces
 	
     assign b0 = inputB[0] ^ mode;
     assign b1 = inputB[1] ^ mode;
     assign b2 = inputB[2] ^ mode;
     assign b3 = inputB[3] ^ mode;
+	assign b4 = inputB[4] ^ mode;
+    assign b5 = inputB[5] ^ mode;
+    assign b6 = inputB[6] ^ mode;
+    assign b7 = inputB[7] ^ mode;
+	assign b8 = inputB[8] ^ mode;
+    assign b9 = inputB[9] ^ mode;
+    assign b10 = inputB[10] ^ mode;
+    assign b11 = inputB[11] ^ mode;
+	assign b12 = inputB[12] ^ mode;
+    assign b13 = inputB[13] ^ mode;
+    assign b14 = inputB[14] ^ mode;
+    assign b15 = inputB[15] ^ mode;
 
 	FullAdder FA0(inputA[0],b0,mode,c1,sum[0]);
 	FullAdder FA1(inputA[1],b1,  c1,c2,sum[1]);
 	FullAdder FA2(inputA[2],b2,  c2,c3,sum[2]);
 	FullAdder FA3(inputA[3],b3,  c3,c4,sum[3]);
+	FullAdder FA4(inputA[4],b4,	 c4,c5,sum[4]);
+	FullAdder FA5(inputA[5],b5,  c5,c6,sum[5]);
+	FullAdder FA6(inputA[6],b6,  c6,c7,sum[6]);
+	FullAdder FA7(inputA[7],b7,  c7,c8,sum[7]);
+	FullAdder FA8(inputA[8],b8,  c8,c9,sum[8]);
+	FullAdder FA9(inputA[9],b9,  c9,c10,sum[9]);
+	FullAdder FA10(inputA[10],b10,  c10,c11,sum[10]);
+	FullAdder FA11(inputA[11],b11,  c11,c12,sum[11]);
+	FullAdder FA12(inputA[12],b12,  c12,c13,sum[12]);
+	FullAdder FA13(inputA[13],b13,  c13,c14,sum[13]);
+	FullAdder FA14(inputA[14],b14,  c14,c15,sum[14]);
+	FullAdder FA15(inputA[15],b15,  c15,c16,sum[15]);
 
-	assign carry=c4;
-	assign overflow=c4^c3;
+	assign carry=c16;
+	assign overflow=c16^c15;
  
 endmodule
 
@@ -73,11 +97,11 @@ module DEC(binary,onehot);
 endmodule;
 
 module MUX(channels, select, b);
-input [15:0][3:0] channels;
+input [127:0][31:0] channels;
 input      [15:0] select;
-output      [3:0] b;
-wire  [15:0][3:0] channels;
-wire        [3:0] b;
+output      [31:0] b;
+wire  [127:0][31:0] channels;
+wire        [31:0] b;
 
 
 	assign b = ({16{select[15]}} & channels[15]) | 
@@ -121,9 +145,9 @@ wire carry;
 wire overflow;
 
 //Multiplexer
-wire [15:0][3:0] channels;
+wire [127:0][31:0] channels;
 wire [15:0] onehotMux;
-wire [3:0] b;
+wire [31:0] b;
 
 DEC DecAlpha(command,onehotMux);
 ADD_SUB nept(inputA,inputB,mode,sum,carry,overflow);
@@ -172,15 +196,15 @@ module TestBench();
   reg [7:0] charA;
   
   initial begin
-    assign inputA  = 255;
-	assign inputB  = 127;
+    assign inputA  = 32000;
+	assign inputB  = 16000;
 	assign command = 1;
 	
 
 	#10;
 	$display("[Input A:%6d, Input B:%6d][Add:%b][Output:%10d, Error: %b]",inputA,inputB,command,result,error);
   
-   	assign command = 2;
+	assign command = 2;
 	#10;
 
 	$display("[Input A:%6d, Input B:%6d][Sub:%b][Output:%10d, Error: %b]",inputA,inputB,command,result,error);
